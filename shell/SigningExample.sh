a632fcda9a1b7d4534b4ec3069f42ccf9858c169
kcberg <kc.berg@gmail.com>
2013-10-18 09:26:35 -0600

    quite that call

--- shell/SigningExample.sh
+++ shell/SigningExample.sh
@@ -11,7 +11,7 @@ now=`date -u "+%a, %d %h %Y %H:%M:%S GMT"`;
 signstr="GET /api/systems/${systemKey} HTTP/1.1\ndate: ${now}"
 signature=`printf "$signstr" | openssl dgst -sha256 -sign /opt/jc/client.key | openssl enc -e -a | tr -d '\n'` ;
 
-curl -iv \
+curl -iq \
   -H "Accept: application/json" \
   -H "Date: ${now}" \
   -H "Authorization: Signature keyId=\"system/${systemKey}\",headers=\"request-line date\",algorithm=\"rsa-sha256\",signature=\"${signature}\"" \
